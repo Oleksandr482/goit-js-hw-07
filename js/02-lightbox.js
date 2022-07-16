e2c1a2d93eb65703c4633b4908749df4e6d7ffc8
Oleksandr Demianenko <oleksandr.dem89@gmail.com>
2022-07-16 22:38:34 +0300

    задача 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const refs = {
+    gallery: document.querySelector('.gallery'),
+}
 
-console.log(galleryItems);
+createGalleryItems();
+
+refs.gallery.addEventListener('click', onGalleryItemClick)
+
+function onGalleryItemClick(evt) {
+  evt.preventDefault();
+
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+  
+  new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+    })
+};
+
+function createGalleryItems() {
+    const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`
+}).join('')
+    refs.gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+};
+
